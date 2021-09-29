12cfd9fa09f36942a4e265226ffa79aa5ff9818b
Jacob Kent <jacobgkent@gmail.com>
2021-09-29 19:08:50 -0400

    Added test imprelemtation for main

--- OOP3200-F2020-Lesson4/Main.cpp
+++ OOP3200-F2020-Lesson4/Main.cpp
@@ -1,8 +1,35 @@
-
+/*
+ * Name: Jacob Kent
+ * Student #: 100609784
+ * Date: Sept 29, 2021
+ * Project: ICE 3
+ * Using files from Tom Tsiliopoulos
+ *
+ *
+ */
 #include <iostream>
+
+#include "Person.h"
+#include "Professor.h"
+#include "Student.h"
 
 int main()
 {
-    \
+    Person person = Person("Jane", "Dough", 22);
+    person.SaysHello();
+
+    std::cout << person.ToString() << std::endl;
+
+    Student student = Student("Jacob", "Kent", 23, "100609784");
+    student.SaysHello();
+    student.Studies();
+
+    std::cout << student.ToString() << std::endl;
+
+    Professor professor = Professor("Bernardo", "de la Paz", 68, "Sherlock123");
+    professor.SaysHello();
+    professor.Teaches();
+
+    std::cout << professor.ToString() << std::endl;
 }
 
